c50a6f4cd294402ff8425f39d8cc64785c240b59
jzhuan1x <jinfengx.zhuang@intel.com>
2018-09-09 21:11:19 +0800

    enhance DBG; resolve signal.h conflict

--- source/include/simulator.h
+++ source/include/simulator.h
@@ -4,20 +4,23 @@
 // standard c
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 // linux platform
 #include <time.h>
 #include <assert.h>
 
 // internal
-#include <signal.h>
+#include "signal.h"
 
 #define VERSION "0.0.0"
 
-extern unsigned int clock_count;
+#define MACHINE_WIDTH	(32)
+
+extern unsigned int sys_clock_count;
 
 #ifdef DEBUG
-#define DBG(fmt, ...) printf("[Simulator] [%d] " fmt, clock_count, ##__VA_ARGS__);
+#define DBG(fmt, ...) printf("[Simulator] [%d] " fmt, sys_clock_count, ##__VA_ARGS__);
 #else
 #define DBG(fmt, ...)
 #endif
