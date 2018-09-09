74186f92297af9ac1b69900bc6fd95e21a211de5
jzhuan1x <jinfengx.zhuang@intel.com>
2018-09-10 02:12:03 +0800

    armv7m: add volatile for fp registers

--- source/include/bitops.h
+++ source/include/bitops.h
@@ -7,17 +7,17 @@
 
 #define BITMASK(width)			(BIT(width) - 1)
 
-#define BITCLR(reg,offset,width)					\
-	((reg) & (~(BITMASK(width) << (offset))))
+#define BITCLR(reg, b, a)						\
+	(reg) = ((reg) & (~(BITMASK((b) - (a) + 1) << (a))))
 
-#define BITSET(reg, offset, width, value)				\
+#define BITSET(reg, b, a, value)					\
 	do {								\
-		assert(BITMASK(width) >= value);			\
-		reg = 	(BITCLR(reg, offset, width)) |			\
-			(((value) & BITMASK(width)) << (offset));	\
+		assert(BITMASK(b - a + 1) >= value);			\
+		reg = 	(BITCLR(reg, b, a)) |				\
+			(((value) & BITMASK(b - a + 1)) << (a));	\
 	} while (0)
 
-#define BITGET(reg, offset, width)					\
-	(((reg) >> (offset)) & BITMASK(width))
+#define BITGET(reg, b, a)						\
+	(((reg) >> (a)) & BITMASK(b - a + 1))
 
 #endif
